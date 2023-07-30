e0365b8f3e40793928a4b561dc1fa9f07b388875
mahmoud adel <mahmoudadel5556@gmail.com>
2023-07-30 18:13:01 +0300

    final code
    - i made it ^_^

--- app/src/main/java/com/example/compose_application/todo/ListViewTask.kt
+++ app/src/main/java/com/example/compose_application/todo/ListViewTask.kt
@@ -1,63 +1,15 @@
 package com.example.compose_application.todo
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import kotlinx.coroutines.launch
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun TaskList() {
-//
-//    val ongoingTasks = remember { mutableStateOf( defaultTasks.filter { !it.isComplete }) }
-//    val completedTasks = remember { mutableStateOf(defaultTasks.filter { it.isComplete }) }
-//
-//
-//    LazyColumn {
-//        item {
-//            if (ongoingTasks.value.isNotEmpty()){
-//                Text(text = "Ongoing Tasks", fontSize =40.sp)
-//                for (task in ongoingTasks.value){
-//                    TaskCard(task = task){
-//                        ongoingTasks.value =  defaultTasks.filter { !it.isComplete}
-//                        completedTasks.value = defaultTasks.filter { it.isComplete}
-//                    }
-//                }
-//            }
-//
-//        }
-//        item {
-//            if (completedTasks.value.isNotEmpty()){
-//                Text(text = "Completed Tasks",  fontSize =40.sp)
-//                for (task in completedTasks.value){
-//                    TaskCard(task = task){
-//                        ongoingTasks.value =  defaultTasks.filter { !it.isComplete}
-//                        completedTasks.value = defaultTasks.filter { it.isComplete}
-//
-//                    }
-//                }
-//            }
-//        }
-//
-//    }
-//
-//
-//}
-//
 
 
 @Preview(showBackground = true)
